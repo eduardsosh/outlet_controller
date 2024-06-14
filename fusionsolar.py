0a7eba4d5d806bed637ffaef1c74631b9f0de93b
Eduards <eduardsosh@gmail.com>
2024-06-14 15:08:27 +0300

    Finished thread based selenium

--- fusionsolar.py
+++ fusionsolar.py
@@ -7,31 +7,47 @@ from selenium.webdriver.firefox.options import Options
 import os
 import solar_logger
 
-import asyncio
-from concurrent.futures import ThreadPoolExecutor
-
+import threading
+import time
 
 class KioskApp:
-    def __init__(self, DEBUG=False):
-        await 
+    def __init__(self, DEBUG=False): 
+        self.active = True       
+        print("Starting selenium browser!")
+        self.driver_thread = threading.Thread(target=self.start_browser)
+        self.driver_thread.start()
 
-
-    async def launch_browser(self):
-        try:
-            logger = solar_logger.Logger()
-        except Exception as e:
-            return
-        
+    def start_browser(self):
+        print("starting thread")
         kiosk_link = os.environ.get('SOLAR_LINK')
         if kiosk_link == None:
             print("No kiosk link found!")
             return
-        
-        options = Options()
-        if not DEBUG:
-            options.add_argument("--headless")
 
-    def get_power_prod(self):
+        options = Options()
+        options.add_argument("--headless")
+        options.add_argument("--detach")
+        self.driver = webdriver.Firefox(options=options)
+
+        self.driver.get(kiosk_link)
+
+        WebDriverWait(self.driver, 10).until(EC.text_to_be_present_in_element((By.CLASS_NAME, "station-name"),"Zemenu19"))
+
+        element = self.driver.find_element(By.CSS_SELECTOR , ".nco-kiosk-overview-data")
+
+        print(f"Started driver, current prod: {element.text}")
+        while self.active:
+            time.sleep(1)
+
+    def get_prod(self):
+        element = self.driver.find_element(By.CSS_SELECTOR , ".nco-kiosk-overview-data")
+        production_kw = element.text.replace("kW","")
+        production_kw = float(production_kw)
+        return production_kw
+    
+    def stop_driver(self):
+        self.active = False
+        
 
 
         
@@ -41,36 +57,11 @@ class KioskApp:
 
 
 def main():
-
-
-
-
-
-    #TODO Add asynchronus get of the power generation and other data
-
-    try:
-        driver = webdriver.Firefox(options=options)
-        driver.get(kiosk_link)
-
-        WebDriverWait(driver, 10).until(EC.text_to_be_present_in_element((By.CLASS_NAME, "station-name"),"Zemenu19"))
-
-        element = driver.find_element(By.CSS_SELECTOR , ".nco-kiosk-overview-data")
-        
-        production_kw = element.text.replace("kW","")
-        production_kw = float(production_kw)
-
-        print(production_kw)
-
-
-
-    except Exception as e:
-        print(e)
-    finally:
-        driver.quit()
-
-
-
-    
+    fusionApp = KioskApp()
+    time.sleep(10)
+    print(fusionApp.get_prod())
+    time.sleep(10)
+    print(fusionApp.get_prod())
     
 
 
